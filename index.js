8ca10c7e6cb889f90e47f65434a782b5cb53df33
NicolasSales0101 <nicolascavalcante0101@gmail.com>
2021-11-02 16:31:47 -0400

    Adição da rota /sobre e do seu arquivo html

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
-const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
-const path = require('path')
+    const express = require('express');
+    const app = express();
+    const server = require('http').createServer(app);
+    const path = require('path')
 
-// indicando aa pastas a serem usadas
-    app.use(express.static(path.join(__dirname,'public')));
+// Indicando aa pastas a serem usadas
+
+    app.use(express.static(path.join(__dirname, 'public')));
     app.set('views', path.join(__dirname, 'public'));
     app.engine('html', require('ejs').renderFile);
     app.set('view engine', 'html');
 
 // Rotas
 
-    app.use('/', (req, res) => {
-        res.render('pages/index.html');
+    app.get('/', (req, res) => {
+        res.render('pages/index'); 
     });
 
-    
+    app.get('/sobre', (req, res) => {
+        res.render('pages/sobre');
+    })
+                    
 // Abrindo o server
+
     server.listen(8081, () => {
         console.log('Servidor rodando 🔄');
     });
+
+
+
+
+ 
+ 
